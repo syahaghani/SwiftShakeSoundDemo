98559d26ab2c383d15927c917abfff49eafc382a
user11 <user11@caspian11s-imac.local>
2015-12-18 10:55:26 +0800

    Initial Commit

---
+++ ShakeSoundDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShakeSoundDemo
+//
+//  Created by user11 on 18/12/2015.
+//  Copyright © 2015 user11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
